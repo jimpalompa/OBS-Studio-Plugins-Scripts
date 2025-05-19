34faa56b7ff9f48cbbaca2d67006dc808ea6dcd0
jimpalompa <43999535+jimpalompa@users.noreply.github.com>
2025-05-19 13:53:52 +0200

    Update youtube-live-chat-url-updater.lua
    Added option to select delay in seconds

--- youtube-live-chat-url-updater.lua
+++ youtube-live-chat-url-updater.lua
@@ -1,14 +1,15 @@
 obs = obslua
 
-g_channel_url = ""
-g_source_name1 = "" -- Alphabet only
-g_source_name2 = ""
-g_source_name3 = ""
-g_source_name4 = ""
-g_source_name5 = ""
+j_channel_url = ""
+j_delay_seconds = 30
+j_source_name_1 = ""
+j_source_name_2 = ""
+j_source_name_3 = ""
+j_source_name_4 = ""
+j_source_name_5 = ""
 
 function get_video_id()
-  local handle = io.popen("curl -s "..g_channel_url)
+  local handle = io.popen("curl -s "..j_channel_url)
   local stdout = handle:read("*a")
   result = string.match(stdout, "\"videoRenderer\":{\"videoId\":\"[^\"]+\"")
   if result == nil then
@@ -33,7 +34,7 @@ function confirm_curl()
 end
 
 function update_live_url()
-  if g_channel_url == "" then
+  if j_channel_url == "" then
     print("Error: Channel URL is not set")
     return
   end
@@ -45,15 +46,15 @@ function update_live_url()
     end
     return
   end
-  print(g_channel_url)
+  print(j_channel_url)
   print(video_id)
 
   source_name_list = {
-    g_source_name1,
-    g_source_name2,
-    g_source_name3,
-    g_source_name4,
-    g_source_name5
+    j_source_name_1,
+    j_source_name_2,
+    j_source_name_3,
+    j_source_name_4,
+    j_source_name_5
   }
 
   for i = 1, #source_name_list do
@@ -75,38 +76,47 @@ function button(props, p)
   return false
 end
 
+function script_defaults(settings)
+  obs.obs_data_set_default_string(settings, "channel_url", "https://www.youtube.com/@Example_Handle/streams")
+  obs.obs_data_set_default_int(settings, "delay_seconds", 30)
+  obs.obs_data_set_default_string(settings, "source_name_1", "Example source name")
+end
+
 function script_update(settings)
-  g_channel_url = obs.obs_data_get_string(settings, "channel_url")
-  g_source_name1 = obs.obs_data_get_string(settings, "source_name1")
-  g_source_name2 = obs.obs_data_get_string(settings, "source_name2")
-  g_source_name3 = obs.obs_data_get_string(settings, "source_name3")
-  g_source_name4 = obs.obs_data_get_string(settings, "source_name4")
-  g_source_name5 = obs.obs_data_get_string(settings, "source_name5")
+  j_channel_url = obs.obs_data_get_string(settings, "channel_url")
+  j_delay_seconds = obs.obs_data_get_int(settings, "delay_seconds")
+  j_delay_milliseconds = (obs.obs_data_get_int(settings, "delay_seconds") * 1000)
+  j_source_name_1 = obs.obs_data_get_string(settings, "source_name_1")
+  j_source_name_2 = obs.obs_data_get_string(settings, "source_name_2")
+  j_source_name_3 = obs.obs_data_get_string(settings, "source_name_3")
+  j_source_name_4 = obs.obs_data_get_string(settings, "source_name_4")
+  j_source_name_5 = obs.obs_data_get_string(settings, "source_name_5")
 end
 
 function script_properties()
   local props = obs.obs_properties_create()
   obs.obs_properties_add_text(props, "channel_url", "Channel URL", obs.OBS_TEXT_DEFAULT)
-  obs.obs_properties_add_text(props, "source_name1", "Source 1", obs.OBS_TEXT_DEFAULT)
-  obs.obs_properties_add_text(props, "source_name2", "Source 2", obs.OBS_TEXT_DEFAULT)
-  obs.obs_properties_add_text(props, "source_name3", "Source 3", obs.OBS_TEXT_DEFAULT)
-  obs.obs_properties_add_text(props, "source_name4", "Source 4", obs.OBS_TEXT_DEFAULT)
-  obs.obs_properties_add_text(props, "source_name5", "Source 5", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_int(props, "delay_seconds", "Update delay (s)", 0, 3600, 1)
+  obs.obs_properties_add_text(props, "source_name_1", "Source 1", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "source_name_2", "Source 2", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "source_name_3", "Source 3", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "source_name_4", "Source 4", obs.OBS_TEXT_DEFAULT)
+  obs.obs_properties_add_text(props, "source_name_5", "Source 5", obs.OBS_TEXT_DEFAULT)
   obs.obs_properties_add_button(props, "button", "Update chat URL", button)
   return props
+end
+
+function on_event(event)
+  if event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTING then
+    print("Updating YouTube chat URLs in " .. j_delay_seconds .. " seconds..")
+    obs.timer_add(load, j_delay_milliseconds)
+  end
 end
 
 function load()
   print("Updating YouTube chat URLs")
   update_live_url()
   obs.remove_current_callback()
-end
-
-function on_event(event)
-  if event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTING then
-    print("Updating YouTube chat URLs in 60 seconds..")
-    obs.timer_add(load, 60000)
-  end
 end
 
 function script_load(settings)
